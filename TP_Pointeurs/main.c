925237b8c0525e70357d3acf9e365134a666739a
Aylia <aylia.sabadie@student.junia.com>
2025-01-17 15:17:33 +0100

    Etape2 terminée.

--- TP_Pointeurs/main.c
+++ TP_Pointeurs/main.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include "header.h"
 
-int main() {
-	Menu menu = { "appel" };
-	Application appli = {"Instagram",{&menu}, 1};
+void displayMenu(const Application* app){
+	printf("--- %s ---\n",app->nom);
+	for (int i = 0; i < app->nb_menus; i++) {
+		printf("%d. %s\n",i+1, app->menus[i].option);
+	}
+}
 
-	printf("%s %s %s %d", menu.option, appli.nom, appli.menus[0].option, appli.nb_menus);
+int main() {
+	Menu menu1 = { "Regarder une photo" };
+	Menu menu2 = { "Prendre une photo" };
+	Menu photoMenus[] = {menu1, menu2};
+	Application photoApp = {"Photos",photoMenus, 2};
+
+	displayMenu(&photoApp);
+
 	return 0;
 }
