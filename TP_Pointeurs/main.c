31aab0951b91095a6b08b8909b54f01961bf8237
Aylia <aylia.sabadie@student.junia.com>
2025-01-17 14:15:22 +0100

    Test du deuxième exemple. Le premier affichait une erreur mais celui-ci fonctionne, on a changé la valeur d'origine de a grâce à son pointeur p.

--- TP_Pointeurs/main.c
+++ TP_Pointeurs/main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-	const int a = 4;
-	a = 5;
+	int a = 2;
+	printf("%p", &a);
+	int* p = &a;
+	printf("\n%p", p);
+	printf("\n%p", &p);
+	*p = 5;
+	printf("\n%d", a);
+
+	return 0;
 }
