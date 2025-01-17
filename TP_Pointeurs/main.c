f5ff40b4c83574b42ef7eae2fb1774b4414ed6b5
Aylia <aylia.sabadie@student.junia.com>
2025-01-17 14:28:23 +0100

    Test de la fonction avec les pointeurs : on trouve bien le minimum et le maximum dans le tableau.

--- TP_Pointeurs/main.c
+++ TP_Pointeurs/main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void min_max(int tab[], int size, int* min, int* max)
+{
+	if (size < 1) {
+		return;
+	}
+	*min = tab[0];
+	*max = tab[0];
+	for (int i = 0; i < size; i++) {
+		if (*min > tab[i]) {
+			*min = tab[i];
+		}
+		if (*max < tab[i]) {
+			*max = tab[i];
+		}
+	}
+}
+
 int main() {
-	int a = 2;
-	printf("%p", &a);
-	int* p = &a;
-	printf("\n%p", p);
-	printf("\n%p", &p);
-	*p = 5;
-	printf("\n%d", a);
+	int min = 0;
+	int max = 0;
+	int tableau[7] = {12, 2 , 8 , -7 , 15 , 2};
+	min_max(tableau, 6, &min, &max);
+	printf("min = %d / max = %d", min, max);
 
 	return 0;
 }
