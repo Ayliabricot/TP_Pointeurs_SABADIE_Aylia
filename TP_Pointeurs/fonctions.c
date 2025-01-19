5a5e67e6f8efd1dd0e130fd894872ef1f500de2c
Aylia <aylia.sabadie@student.junia.com>
2025-01-19 22:55:40 +0100

    J'ai essaye de faire l'etape 5 mais je n'ai pas réussi à la faire fonctionner. J'ai vraiment essayé tout ce à quoi j'ai pu penser, j'ai fait des recherches sur internet avec des forums notamment car je ne trouvais pas comment changer les const char* au sein des structs en passant par une fonction... Je ne comprends toujours pas comment nous devions faire, j'attends votre correction pour voir comment il fallait faire.

--- TP_Pointeurs/fonctions.c
+++ TP_Pointeurs/fonctions.c
@@ -20,7 +20,6 @@ void runApplication(const Application* app) {
 
 		if (choix == 0) {
 			system("cls");
-			printf("Vous avez quitte.");
 			continuer = 0;
 		}
 		else if (choix > 0 && choix < app->nb_menus + 1) {
@@ -30,22 +29,82 @@ void runApplication(const Application* app) {
 }
 
 void runPhone(Application* app1, Application* app2) {
-	int choix;
+	int choix = 10;
+	while (choix > 0)
+	{
+		printf("--- Applications ---\n");
+		printf("1. %s\n2. %s\n3. Modifier le menu de '%s'\n4. Modifier l'application '%s'\n0. Quitter\nVotre action : ", app1->nom, app2->nom, app1->nom, app2->nom);
+		scanf_s("%d", &choix);
 
-	printf("--- Applications ---\n");
-	printf("1. %s\n2. %s\n0. Quitter\nVotre action : ", app1->nom, app2->nom);
-	scanf_s("%d", &choix);
+		if (choix == 1) {
+			system("cls");
+			runApplication(app1);
+		}
+		else if (choix == 2) {
+			system("cls");
+			runApplication(app2);
+		}
+		else if (choix == 3) {
+			system("cls");
+			displayMenu(app1);
+			int menuIndex;
+			char newOption[100];
+			printf("Choisissez l'index du menu a modifier : ");
+			scanf_s("%d", &menuIndex);
+			printf("Choisissez la nouvelle option : ");
+			getchar();
+			fgets(newOption, sizeof(newOption), stdin);
+			newOption[strcspn(newOption, "\n")] = 0;
+			updateMenu(app1, menuIndex, strdup(newOption));
+		}
+		else if (choix == 4) {
+			system("cls");
+			char nomApp[100];
+			char option[100];
+			int i = 1;
+			int nbMenus = 0;
+			Menu nouveauMenus[100];
 
-	if (choix == 1) {
-		system("cls");
-		runApplication(app1);
+			printf("Quel est le nom de l'application : ");
+			getchar();
+			fgets(nomApp, sizeof(nomApp), stdin);
+			nomApp[strcspn(nomApp, "\n")] = 0;
+
+			while (1) {
+				printf("Choisissez l'option %d du menu (ou 0 pour quitter) : ", i);
+				fgets(option, sizeof(option), stdin);
+				option[strcspn(option, "\n")] = 0;
+				if (option[0] == '0') {
+					break;
+				}
+				nouveauMenus[i - 1].option = strdup(option);
+				i++;
+			}
+			nbMenus = i - 1;
+			updateApplication(app2, strdup(nomApp), nouveauMenus, nbMenus);
+		}
+		else if (choix == 0) {
+			system("cls");
+			printf("Vous avez quitte.");
+		}
 	}
-	else if (choix == 2) {
-		system("cls");
-		runApplication(app2);
+}
+
+void updateMenu(Application* app, int menuIndex, const char* newOption) {
+	while (1) {
+		if (menuIndex <= 0 || menuIndex > app->nb_menus) {
+			printf("L'index est invalide : veuillez entrer un nombre entre 1 et %d\n", app->nb_menus);
+			continue;
+		}
+		app->menus[menuIndex - 1].option = newOption;
+		printf("L'option a ete mise a jour.\n");
+		break;
 	}
-	else if (choix == 0) {
-		system("cls");
-		printf("Vous avez quitte.");
-	}
+}
+
+void updateApplication(Application* app, const char* newName, Menu* newMenus, int newMenuCount) {
+	app->nom = newName;
+	app->menus = newMenus;
+	app->nb_menus = newMenuCount;
+	printf("L'application a ete mise a jour.\n");
 }
--- TP_Pointeurs/header.h
+++ TP_Pointeurs/header.h
@@ -14,5 +14,7 @@ typedef struct {
 void displayMenu(const Application* app);
 void runApplication(const Application* app);
 void runPhone(Application* app1, Application* app2);
+void updateMenu(Application* app, int menuIndex, const char* newOption);
+void updateApplication(Application* app, const char* newName, Menu* newMenus, int newMenuCount);
 
 #endif
